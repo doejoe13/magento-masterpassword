049ad0ff138dcd77f2105547c9076b534f287735
doejoe13 <doejoe13@gmail.com>
2019-04-17 21:10:29 +0200

    Update Compatibility.php

--- app/code/local/PAJ/MasterPassword/Customer/Model/Customer/Compatibility.php
+++ app/code/local/PAJ/MasterPassword/Customer/Model/Customer/Compatibility.php
@@ -2,6 +2,12 @@
 if (Mage::getConfig()->getModuleConfig('Zestard_Customerattribute')->is('active', 'true'))
     {
       class PAJ_MasterPassword_Customer_Model_Customer_Compatibility extends Zestard_Customerattribute_Model_Customer
+      {
+      
+      }
     } else {
-       class PAJ_MasterPassword_Customer_Model_Customer_Compatibility extends Mage_Customer_Model_Customer     
+       class PAJ_MasterPassword_Customer_Model_Customer_Compatibility extends Mage_Customer_Model_Customer
+       {
+       
+       }
     }
